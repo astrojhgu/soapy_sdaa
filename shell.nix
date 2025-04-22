33b9587915c6f062733e2ac978e2b1c777a5d534
GU Junhua <jhgu@nao.cas.cn>
2025-04-22 10:21:08 +0800

    modified:   shell.nix

--- shell.nix
+++ shell.nix
@@ -1,32 +1,54 @@
 # Run with `nix-shell cuda-shell.nix`
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-   name = "cuda-env-shell";
-   buildInputs = with pkgs; [
-     git gitRepo gnupg autoconf curl
-     procps gnumake util-linux m4 gperf unzip
-     cudatoolkit linuxPackages.nvidia_x11
-     libGLU libGL nvtopPackages.nvidia
-     cudaPackages.cuda_cudart.all
-     xorg.libXi xorg.libXmu freeglut
-     xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-     ncurses5 stdenv.cc binutils
-     gdb
-     boost.all
-     soapysdr
-     yaml-cpp
-     pkg-config
-     gnuradio
-     gqrx
-     sdrangel
-     sigdigger
-     sdrpp
-   ];
-   shellHook = ''
-      export CUDA_PATH=${pkgs.cudatoolkit}
-      export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
-      export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-      export EXTRA_CCFLAGS="-I/usr/include"
-      export SOAPY_SDR_PLUGIN_PATH=$PWD
-   '';          
+  name = "cuda-env-shell";
+  buildInputs = with pkgs; [
+    git
+    gitRepo
+    gnupg
+    autoconf
+    curl
+    procps
+    gnumake
+    util-linux
+    m4
+    gperf
+    unzip
+    cudatoolkit
+    linuxPackages.nvidia_x11
+    libGLU
+    libGL
+    nvtopPackages.nvidia
+    cudaPackages.cuda_cudart.all
+    xorg.libXi
+    xorg.libXmu
+    freeglut
+    xorg.libXext
+    xorg.libX11
+    xorg.libXv
+    xorg.libXrandr
+    zlib
+    ncurses5
+    stdenv.cc
+    binutils
+    gdb
+    boost.all
+    soapysdr
+    yaml-cpp
+    pkg-config
+    gnuradio
+    gqrx
+    sdrangel
+    sigdigger
+    sdrpp
+
+    (python3.withPackages (ps: with ps; [numpy scipy matplotlib soapysdr ]))
+  ];
+  shellHook = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+    export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+    export SOAPY_SDR_PLUGIN_PATH=$PWD
+  '';
 }
